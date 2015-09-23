30a65f7442fe5dec355b57917b28c004ba7efd7d
iberescu <iberescu@gmail.com>
2015-09-24 00:05:33 +0200

    Update Controller_Error.php

--- controllers/Controller_Error.php
+++ controllers/Controller_Error.php
@@ -9,8 +9,6 @@ class Controller_Error extends Controller {
 	function index($message = '') 
 	{
 		header("HTTP/1.0 404 Not Found");
-
-		$this->loadModel('Error');
 	
 		if (!DEBUG_ENVIRONMENT)
 		{
@@ -19,7 +17,6 @@ class Controller_Error extends Controller {
 		$this->view->assign('title','404 Error')
 				   ->assign('keywords','error,404')
 				   ->assign('description','404 error')
-				   ->assign('latest',$this->model->getLatest())
 				   ->display('error/index');
 	}
 }
